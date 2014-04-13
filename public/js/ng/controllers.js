69b804a5f758aa8631a6a5c701cd8f817fcf8283
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2014-04-13 16:17:14 +0300

    Add angular

---
+++ public/js/ng/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+    .controller('CreateEventCtrl', [function() {
+
+    }])
+    .controller('MyCtrl1', [function() {
+
+    }])
+    .controller('MyCtrl2', [function() {
+
+    }]);
---
+++ public/js/ng/directives.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Directives */
+
+
+angular.module('myApp.directives', []).
+    directive('my directive', [function() {
+        return function(scope, elm, attrs) {
+        };
+    }]);
