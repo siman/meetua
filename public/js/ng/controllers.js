7e03b34954daee75b4f37a45f00c961ab0336ada
Alex Siman <aleksandr.siman@gmail.com>
2014-04-13 17:50:03 +0300

    added selectable activities

--- public/js/ng/controllers.js
+++ public/js/ng/controllers.js
@@ -6,8 +6,21 @@ angular.module('myApp.controllers', [])
     .controller('CreateEventCtrl', [function() {
 
     }])
-    .controller('MyCtrl1', [function() {
+    .controller('SelectEventCtrl', ['$scope', function($scope) {
+      $scope.selectedActs = {
+        bike: false,
+        running: false,
+        workout: false,
+        hiking: false,
+        photo: false,
+        en: false,
+        code: false
+      };
 
+      $scope.onActClick = function(actName) {
+        $scope.selectedActs[actName] = !$scope.selectedActs[actName];
+        console.log("selectedActs", $scope.selectedActs);
+      }
     }])
     .controller('MyCtrl2', [function() {
 
