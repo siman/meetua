99afe7511452d1c1852ed76905dfee7e255a2b7e
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2014-04-19 19:22:04 +0300

    Embed angular-strap templates in the single js file

--- public/js/application.js
+++ public/js/application.js
@@ -22,7 +22,6 @@
 //= require lib/ng-bs-daterangepicker-0.0.1
 //= require lib/angular-google-maps-1.0.18.min
 //= require lib/angular-strap-2.0.1
-//= require lib/angular-strap-2.0.1.tpl
 //= require config
 //= require_tree ng
 //= require lib/ng-directives
