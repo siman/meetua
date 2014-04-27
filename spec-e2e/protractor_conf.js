f629abb75faebbece8ba158c919fe3585cf92f73
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2014-04-27 16:32:14 +0300

    #11 Move e2e specs into separate dir named 'spec-e2e'.

--- spec-e2e/protractor_conf.js
+++ spec-e2e/protractor_conf.js
@@ -1,17 +1,19 @@
+var baseUrl = 'http://127.0.0.1:3000/';
+
 // An example configuration file.
 exports.config = {
   // Do not start a Selenium Standalone sever - only run this using chrome.
   //  chromeOnly: true,
   //  chromeDriver: './node_modules/protractor/selenium/chromedriver',
   seleniumAddress: 'http://0.0.0.0:4444/wd/hub',
-  baseUrl: 'http://127.0.0.1:3000/',
+  baseUrl: baseUrl,
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: { 'browserName': 'chrome' },
 
   // Spec patterns are relative to the current working directly when
   // protractor is called.
-  specs: ['*.spec.js'],
+  specs: ['*spec.js'],
 
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
--- spec-e2e/simple-spec.js
+++ spec-e2e/simple-spec.js
@@ -7,11 +7,7 @@ describe('homepage', function () {
     browser.get('http://127.0.0.1:3000/');
     ptor = protractor.getInstance();
     browser.get('/event/create');
-    login();
   });
-
-  function login() {
-  }
 
   it('should load the page', function () {
 //    // test goes here
