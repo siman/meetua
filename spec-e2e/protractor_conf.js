24cc7d63a6c9bf838415cd66451f69f56d0b09f7
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2014-05-07 09:59:38 +0300

    Implement login before selenium test.

--- spec-e2e/protractor_conf.js
+++ spec-e2e/protractor_conf.js
@@ -1,4 +1,7 @@
-var baseUrl = 'http://127.0.0.1:3000/';
+"use strict";
+
+var baseUrl = 'http://127.0.0.1:3000';
+var mockUsers = require('../controllers/user-mock-store');
 
 // An example configuration file.
 exports.config = {
@@ -19,5 +22,24 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000
+  },
+
+  onPrepare: function() {
+
+    var loginUser = mockUsers[0];
+    browser.driver.get(baseUrl + '/login');
+
+    browser.driver.findElement(by.id('email')).sendKeys(loginUser.email);
+    browser.driver.findElement(by.id('password')).sendKeys(loginUser.password);
+    browser.driver.findElement(by.xpath("//node()[@type='submit']")).click();
+
+    // Login takes some time, so wait until it's done.
+    // For the test app's login, we know it's done when it redirects to
+    // home page.
+    browser.driver.wait(function() {
+      return browser.driver.getCurrentUrl().then(function(url) {
+        return new RegExp(baseUrl).test(url);
+      });
+    });
   }
 };
