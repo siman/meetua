061c7b24ee8b470cd64f3a6efd8c441e08fe735c
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2014-05-10 23:51:30 +0300

    #11 Remove obsolete shared image service.

--- controllers/event/create-page.js
+++ controllers/event/create-page.js
@@ -1,9 +1,6 @@
 'use strict';
 
-var eventService = require('../../public/js/app/shared/event-service');
-
 module.exports = function(req, res) {
-    console.log('Shared eventService ', eventService.createEvent());
     res.render('event/create', {
         title: 'Создать событие'
     });
