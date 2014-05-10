32a89f87faf799492179ac448804678b5adb2d53
null <serhii.nesteruk@gmail.com>
2014-05-10 13:25:19 +0300

    #16 updated user schema: added receive notification option

--- models/User.js
+++ models/User.js
@@ -21,7 +21,8 @@ var userSchema = new mongoose.Schema({
     gender: { type: String, default: '' },
     location: { type: String, default: '' },
     website: { type: String, default: '' },
-    picture: { type: String, default: '' }
+    picture: { type: String, default: '' },
+		receiveNotifications: { type: Boolean, default: true }
   },
 
   resetPasswordToken: String,
