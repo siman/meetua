d0a9299bd43eeb0a15400d6e26f96ef3024f92b1
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2014-05-10 14:34:46 +0300

    #11 ocd

--- public/js/app/event/create-event-ctrl.js
+++ public/js/app/event/create-event-ctrl.js
@@ -1,39 +1,39 @@
 'use strict';
 
 angular.module('myApp')
-    .controller('CreateEventCtrl', ['$scope', '$http', '$window', 'EventImageService', 'activities',
-    function($scope, $http, $window, EventImageService, activities) {
-        var imageService = $scope.imageService = EventImageService.create({
-          scope: $scope,
-          onAllUploaded: function submitAfterUpload(uploadedImages) {
-            doSendPost(buildReqData(uploadedImages));
-          }
-        });
-        var uploader = $scope.uploader = imageService.uploader;
-        $scope.activities = activities;
-        $scope.event = {};
-        $scope.submit = function() {
-            if (uploader.queue.length > 0) {
-                uploader.uploadAll();
-            } else {
-                doSendPost(buildReqData());
-            }
-        };
-        function buildReqData(uploadedImages) {
-            var reqData = _.extend($scope.event, {
-                images: uploadedImages
-            });
-            console.log('Request data ', reqData);
-            return reqData;
-        }
-        function doSendPost(requestData) {
-            $http.post('/event/create', requestData).success(function(res){
-                console.log('Event is created successfully ', res);
-                var redirectUrl = '/event/' + res.event._id;
-                console.log('Redirecting to ', redirectUrl);
-                $window.location = redirectUrl;
-            }).error(function(err) {
-                    console.error('Failed to create event ', err);
-                });
-        }
-    }]);
+  .controller('CreateEventCtrl', ['$scope', '$http', '$window', 'EventImageService', 'activities',
+  function($scope, $http, $window, EventImageService, activities) {
+    var imageService = $scope.imageService = EventImageService.create({
+      scope: $scope,
+      onAllUploaded: function submitAfterUpload(uploadedImages) {
+        doSendPost(buildReqData(uploadedImages));
+      }
+    });
+    var uploader = $scope.uploader = imageService.uploader;
+    $scope.activities = activities;
+    $scope.event = {};
+    $scope.submit = function() {
+      if (uploader.queue.length > 0) {
+        uploader.uploadAll();
+      } else {
+        doSendPost(buildReqData());
+      }
+    };
+    function buildReqData(uploadedImages) {
+      var reqData = _.extend($scope.event, {
+          images: uploadedImages
+      });
+      console.log('Request data ', reqData);
+      return reqData;
+    }
+    function doSendPost(requestData) {
+      $http.post('/event/create', requestData).success(function(res){
+        console.log('Event is created successfully ', res);
+        var redirectUrl = '/event/' + res.event._id;
+        console.log('Redirecting to ', redirectUrl);
+        $window.location = redirectUrl;
+      }).error(function(err) {
+        console.error('Failed to create event ', err);
+      });
+    }
+  }]);
