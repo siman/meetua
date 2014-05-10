d2930a1527d090be8e94398718eeacd7c347ef6c
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2014-05-10 23:51:49 +0300

    #11 Update event dirty version.

--- public/js/app/event/edit-event-ctrl.js
+++ public/js/app/event/edit-event-ctrl.js
@@ -1,7 +1,41 @@
 'use strict';
 
 angular.module('myApp')
-    .controller('EditEventCtrl', ['$scope', 'activities', function($scope, activities) {
+    .controller('EditEventCtrl', ['$scope', 'activities', 'EventImageService', '$http', function($scope, activities, EventImageService, $http) {
       $scope.event = _myInit.event;
       $scope.activities = activities;
+      var imageService = $scope.imageService = EventImageService.create({
+        scope: $scope,
+        onAllUploaded: function submitAfterUpload(uploadedImages) {
+          doSendPost(buildReqData(uploadedImages));
+        },
+        uploadedImages: $scope.event.images,
+        event: $scope.event
+      });
+      var uploader = $scope.uploader = imageService.uploader;
+      $scope.submit = function() {
+        if (uploader.queue.length > 0) {
+          uploader.uploadAll();
+        } else {
+          doSendPost(buildReqData());
+        }
+      };
+      function buildReqData(uploadedImages) {
+        var images = uploadedImages ? $scope.event.images.concat(uploadedImages) : $scope.event.images;
+        var reqData = _.extend($scope.event, {
+          images: images
+        });
+        console.log('Request data ', reqData);
+        return reqData;
+      }
+      function doSendPost(requestData) {
+        $http.post('/event/save', requestData).success(function(res){
+          console.log('Event is updated successfully ', res);
+          var redirectUrl = '/event/' + res.event._id;
+          console.log('Redirecting to ', redirectUrl);
+          $window.location = redirectUrl;
+        }).error(function(err) {
+          console.error('Failed to create event ', err);
+        });
+        }
   }]);
