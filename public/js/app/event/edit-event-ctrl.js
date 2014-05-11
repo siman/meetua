84b9a36e56a94d8b3b5d8e677a097268f364f9ec
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2014-05-11 11:04:15 +0300

    #11 ocd

--- public/js/app/event/edit-event-ctrl.js
+++ public/js/app/event/edit-event-ctrl.js
@@ -1,32 +1,32 @@
 'use strict';
 
 angular.module('myApp')
-    .controller('EditEventCtrl', ['$scope', 'activities', 'EventImageService', 'EventService',
-    function($scope, activities, EventImageService, EventService) {
-      $scope.event = _myInit.event;
-      $scope.activities = activities;
-      var imageService = $scope.imageService = EventImageService.create({
-        scope: $scope,
-        onAllUploaded: function submitAfterUpload(uploadedImages) {
-          EventService.doSendPost(buildReqData(uploadedImages));
-        },
-        uploadedImages: $scope.event.images,
-        event: $scope.event
-      });
-      var uploader = $scope.uploader = imageService.uploader;
-      $scope.submit = function() {
-        if (uploader.queue.length > 0) {
-          uploader.uploadAll();
-        } else {
-          EventService.doSendPost(buildReqData());
-        }
-      };
-      function buildReqData(uploadedImages) {
-        var images = uploadedImages ? $scope.event.images.concat(uploadedImages) : $scope.event.images;
-        var reqData = _.extend($scope.event, {
-          images: images
-        });
-        console.log('Request data ', reqData);
-        return reqData;
+  .controller('EditEventCtrl', ['$scope', 'activities', 'EventImageService', 'EventService',
+  function($scope, activities, EventImageService, EventService) {
+    $scope.event = _myInit.event;
+    $scope.activities = activities;
+    var imageService = $scope.imageService = EventImageService.create({
+      scope: $scope,
+      onAllUploaded: function submitAfterUpload(uploadedImages) {
+        EventService.doSendPost(buildReqData(uploadedImages));
+      },
+      uploadedImages: $scope.event.images,
+      event: $scope.event
+    });
+    var uploader = $scope.uploader = imageService.uploader;
+    $scope.submit = function() {
+      if (uploader.queue.length > 0) {
+        uploader.uploadAll();
+      } else {
+        EventService.doSendPost(buildReqData());
       }
+    };
+    function buildReqData(uploadedImages) {
+      var images = uploadedImages ? $scope.event.images.concat(uploadedImages) : $scope.event.images;
+      var reqData = _.extend($scope.event, {
+        images: images
+      });
+      console.log('Request data ', reqData);
+      return reqData;
+    }
   }]);
