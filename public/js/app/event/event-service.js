91e3195a2afed744a6b7fd19b892c1b9576bb9d2
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2014-05-11 11:02:48 +0300

    #11 Extract EventService

---
+++ public/js/app/event/event-service.js
@@ -0,0 +1,13 @@
+angular.module('myApp')
+  .service('EventService', ['$http', '$window', function($http, $window) {
+    this.doSendPost = function(requestData) {
+      $http.post('/event/save', requestData).success(function(res){
+        console.log('Event is saved successfully ', res);
+        var redirectUrl = '/event/' + res.event._id;
+        console.log('Redirecting to ', redirectUrl);
+        $window.location = redirectUrl;
+      }).error(function(err) {
+        console.error('Failed to save event ', err);
+      });
+    }
+  }]);
