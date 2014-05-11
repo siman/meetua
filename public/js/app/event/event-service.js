5bdbff70109b19dd24cba72198ce150cd971c729
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2014-05-11 11:28:38 +0300

    #11 Default value for doRedirect.

--- public/js/app/event/event-service.js
+++ public/js/app/event/event-service.js
@@ -3,6 +3,9 @@ angular.module('myApp')
     this.postSave = function(requestData, doRedirect) {
       $http.post('/event/save', requestData).success(function(res){
         console.log('Event is saved successfully ', res);
+        if (_.isUndefined(doRedirect)) {
+          doRedirect = true;
+        }
         if (doRedirect === true) {
           var redirectUrl = '/event/' + res.event._id;
           console.log('Redirecting to ', redirectUrl);
