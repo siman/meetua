eb2c35ec212dd8192a5801ccd6550cc554f886ef
Alex Siman <aleksandr.siman@gmail.com>
2014-05-13 05:33:15 +0300

    Add configs: IS_WINDOWS, IS_LINUX

--- config/app-config.js
+++ config/app-config.js
@@ -6,6 +6,11 @@ var moment = require('moment');
 moment.lang('ru');
 
 module.exports = {
+
+  // http://nodejs.org/api/process.html#process_process_platform
+  IS_WINDOWS: process.platform === 'win32',
+  IS_LINUX: process.platform === 'linux',
+
   MAX_EVENTS_IN_OVERVIEW: 5,
   EVENT_IMG_DIR: './public/upload',
   UPLOAD_DIR: path.join(os.tmpdir(), 'meetua', 'upload'),
