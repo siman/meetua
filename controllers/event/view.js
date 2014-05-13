789ae77cc42ba2c9169795f17553a9f364fe348f
Alex Siman <aleksandr.siman@gmail.com>
2014-05-13 06:39:33 +0300

    fix #40 Edit event panel on top of View event page

--- controllers/event/view.js
+++ controllers/event/view.js
@@ -6,14 +6,18 @@ var _ = require("underscore");
 module.exports = function(req, res) {
   var id = req.params.id;
   store.findById(id, function(err, event) {
-    if (err) console.log('Error while quering by event id', err);
+    if (err) console.log('Error while looking for event by ID', err);
     console.log("id", id);
     console.log("found event", event);
     if (!event) {
       res.status(404);
       res.render("404");
     } else {
-      res.render('event/view', {event: event});
+      res.render('event/view', {
+        event: event,
+        // If current user is an author of event
+        isCurrentUserAnAuthor: event.author && req.user && event.author._id.equals(req.user._id)
+      });
     }
   });
 };
