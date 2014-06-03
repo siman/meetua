ea77f6285f20300d152902af35028f8d5dff6afe
Alex Siman <aleksandr.siman@gmail.com>
2014-06-03 07:27:35 +0300

    Rename applicationCtrl -> AppCtrl

--- public/js/app/common/app-ctrl.js
+++ public/js/app/common/app-ctrl.js
@@ -6,7 +6,7 @@ angular.module('myApp')
    * It holds only currentUser object, so it's available on any controller without injection.
    * Don't add any other objects to it, since we have to keep global objects as little as possible.
    */
-  .controller('applicationCtrl', ['$scope', function($scope) {
+  .controller('AppCtrl', ['$scope', function($scope) {
     $scope.currentUser = _myInit.currentUser;
     console.log("Current user", $scope.currentUser);
   }]);
