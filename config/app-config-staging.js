11e4a4e7fcb66ddc0d701b1b761619000d280d36
MeetUA <meetua@meetua.com>
2014-07-22 13:20:13 -0400

    Separate logs and persistent dir for staging server

--- config/app-config-staging.js
+++ config/app-config-staging.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var PORT = 3001;
 var DOMAIN = 'meetua.com';
 
@@ -7,5 +8,7 @@ module.exports = {
   },
   port: PORT,
   domain: DOMAIN,
-  hostname: 'http://' + DOMAIN + ':' + PORT
+  hostname: 'http://' + DOMAIN + ':' + PORT,
+  LOG_FILE_NAME: 'staging-service.log',
+  PERSISTENT_DATA_DIR: process.env.PERSISTENT_DATA_DIR || path.join(process.cwd(), '../PERSISTENT_DATA_DIR_STAGING')
 };
