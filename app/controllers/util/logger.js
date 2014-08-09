64f615005a989a0e8838323cbcc8bf5cf7c7463b
Alex Siman <aleksandr.siman@gmail.com>
2014-08-10 02:15:55 +0300

    Remove date from logging to reduce logging message and make it more readable

--- app/controllers/util/logger.js
+++ app/controllers/util/logger.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var winston = require('winston');
 var path = require('path');
 var fs = require('fs-extra');
+var moment = require('moment');
 var config = require('../../../config/app-config');
 var logDir = path.join(config.PERSISTENT_DATA_DIR, config.LOG_DIR_NAME);
 
@@ -8,9 +11,19 @@ fs.ensureDir(logDir, function(err) {
   logger.debug(err);
 });
 
+winston.addColors({
+  debug: 'cyan' // Override dark blue by sky blue of cyan.
+});
+
 var logger = new (winston.Logger)({
   transports: [
-    new (winston.transports.Console)({timestamp: true, level: 'debug'}),
+    new (winston.transports.Console)({
+      timestamp: function() {
+        return moment().format('HH:mm:ss.SSS');
+      },
+      level: 'debug',
+      colorize: 'true'
+    }),
     new (winston.transports.DailyRotateFile)({
       filename: logDir + '/' + config.LOG_FILE_NAME,
       datePattern: '.yyyy-MM-dd',
@@ -20,29 +33,27 @@ var logger = new (winston.Logger)({
   ]
 });
 
-module.exports = function (module) {
+module.exports = function(module) {
+  var loggerName = path.basename(module);
 
-  var filename = module;
-  return {
-    debug : function (msg, vars) {
-      logger.debug('[' + filename + ']' + getMsg(arguments));
-    },
-    info : function (msg, vars) {
-      logger.debug('[' + filename + ']' + getMsg(arguments));
-    },
-    warn : function (msg, vars) {
-      logger.warn('[' + filename + ']' + getMsg(arguments));
-    },
-    error : function (msg, vars) {
-      logger.error('[' + filename + ']' + getMsg(arguments));
+  function buildMsg(args) {
+    var msg = '[' + loggerName + ']';
+    for (var i = 0; i < args.length; i++) {
+      msg = msg + ' ' + args[i];
     }
+    return msg;
+  }
+
+  function buildLogger(loggerFunc) {
+    return function() {
+      loggerFunc(buildMsg(arguments));
+    };
+  }
+
+  return {
+    debug: buildLogger(logger.debug),
+    info: buildLogger(logger.info),
+    warn: buildLogger(logger.warn),
+    error: buildLogger(logger.error)
   }
 };
-
-function getMsg(args) {
-  var msg = "";
-  for (var i = 0; i < args.length; i++) {
-    msg = msg + " " + args[i]
-  }
-  return msg;
-}
