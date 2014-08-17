142d16400b715611951efd1c5616b5935b3c5b7b
Alex Siman <aleksandr.siman@gmail.com>
2014-08-18 01:06:07 +0300

    Format code of save event

--- app/controllers/event/save-event.js
+++ app/controllers/event/save-event.js
@@ -41,55 +41,55 @@ function _saveEvent(args, cb) {
   var flashFn = args.flashFn;
 
   if (isCreate) {
-      logger.debug('Create event request', params);
-    } else {
-      logger.debug('Update event request', params);
-    }
+    logger.debug('Create event request', params);
+  } else {
+    logger.debug('Update event request', params);
+  }
 
-    var event;
-    var reqImages = params.images || [];
+  var event;
+  var reqImages = params.images || [];
 
-    if (!isCreate) {
-      Event.findById(params._id, function(err, eventFound) {
-        if (err) cb(500, err); // TODO find out standard way to pass errors when UI will have standard error handler
-        if (!eventFound || !eventFound.author.equals(currentUser._id)) return cb(404, {error: 'Event is not found'});
-        event = eventFound;
+  if (!isCreate) {
+    Event.findById(params._id, function(err, eventFound) {
+      if (err) cb(500, err); // TODO find out standard way to pass errors when UI will have standard error handler
+      if (!eventFound || !eventFound.author.equals(currentUser._id)) return cb(404, {error: 'Event is not found'});
+      event = eventFound;
 
-        // merge images overriding from request
-        var arr = _.partition((reqImages).concat(event.images || []), function(image) {
-          return !_.isUndefined(image._id);
-        });
-        var imagesWithId = _.uniq(arr[0], function(image) {
-          return image._id.toString();
-        });
-        var newImages = arr[1];
-        var allImages = imagesWithId.concat(newImages);
-        verifyLogoCount(allImages, doSave(newImages, imagesWithId));
+      // merge images overriding from request
+      var arr = _.partition((reqImages).concat(event.images || []), function(image) {
+        return !_.isUndefined(image._id);
       });
-    } else {
-      verifyLogoCount(reqImages, doSave(reqImages, []));
-    }
-
-    function verifyLogoCount(images, next) {
-      logger.debug('verifyLogoCount');
-      async.reduce(images, 0, countLogoImages, function(err, logoCount) {
-        if (err) return cb(500, err);
-        if (images.length > 0 && logoCount != 1) {
-          var errorMsg = 'Invalid logo count ' + logoCount;
-          logger.debug(errorMsg);
-          return cb(400, {error: errorMsg});
-        }
-        next();
+      var imagesWithId = _.uniq(arr[0], function(image) {
+        return image._id.toString();
       });
-    }
-    function doSave(newImages, imagesWithId) {
-      return function() {
-        logger.debug('doSave');
-        var saveEventParams = {event: event, imagesWithId: imagesWithId, currentUser: currentUser,
-          isCreate: isCreate, params: params, flashFn: flashFn};
-        async.map(newImages, copyImage, buildAndSaveEvent(saveEventParams, cb));
-      };
-    }
+      var newImages = arr[1];
+      var allImages = imagesWithId.concat(newImages);
+      verifyLogoCount(allImages, doSave(newImages, imagesWithId));
+    });
+  } else {
+    verifyLogoCount(reqImages, doSave(reqImages, []));
+  }
+
+  function verifyLogoCount(images, next) {
+    logger.debug('verifyLogoCount');
+    async.reduce(images, 0, countLogoImages, function(err, logoCount) {
+      if (err) return cb(500, err);
+      if (images.length > 0 && logoCount != 1) {
+        var errorMsg = 'Invalid logo count ' + logoCount;
+        logger.debug(errorMsg);
+        return cb(400, {error: errorMsg});
+      }
+      next();
+    });
+  }
+  function doSave(newImages, imagesWithId) {
+    return function() {
+      logger.debug('doSave');
+      var saveEventParams = {event: event, imagesWithId: imagesWithId, currentUser: currentUser,
+        isCreate: isCreate, params: params, flashFn: flashFn};
+      async.map(newImages, copyImage, buildAndSaveEvent(saveEventParams, cb));
+    };
+  }
 }
 
 function countLogoImages(acc, image, next) {
@@ -110,92 +110,92 @@ function countLogoImages(acc, image, next) {
  * @returns {Function}
  */
 function buildAndSaveEvent(args, cb) {
-    var event = args.event;
-    var imagesWithId = args.imagesWithId;
-    var params = args.params;
-    var flashFn = args.flashFn;
-    var currentUser = args.currentUser;
-    var isCreate = args.isCreate;
-    return function(err, images) {
-        if (err) return cb(400, err);
+  var event = args.event;
+  var imagesWithId = args.imagesWithId;
+  var params = args.params;
+  var flashFn = args.flashFn;
+  var currentUser = args.currentUser;
+  var isCreate = args.isCreate;
+  return function(err, images) {
+    if (err) return cb(400, err);
 
-      delete params.images;
+    delete params.images;
 
-        if (event) {
-          updateEvent();
+    if (event) {
+      updateEvent();
+    } else {
+      createEvent();
+    }
+
+    function createEvent() {
+      var eventObj = _.extend(params, {
+        images: _.map(images, function(image){
+          return new Image(image);
+        }),
+        author: currentUser._id
+      });
+      logger.debug('Creating event ', eventObj);
+      event = new Event(eventObj);
+      event.save(afterSave(event));
+    }
+
+    function updateEvent() {
+      _.extend(event, params, {images: images.concat(imagesWithId)});
+
+      logger.debug('Updating event by id', event._id, event);
+      var respEvent = _.extend({}, event, {_id: event._id});
+      event.save(afterSave(respEvent));
+    }
+
+    function afterSave(event) {
+      return function(err) {
+        if (err) {
+          logger.error("error while saving event", err);
+          return cb(400, {error: err});
+        }
+
+        var onNotifyComplete = function(err, resp) {
+          logger.debug('Notification response: %s, error: %s', resp, err);
+        };
+        if (isCreate) {
+          notificationService.notifyAuthorOnCreate(event, onNotifyComplete)
         } else {
-          createEvent();
+          notificationService.notifyParticipantOnEdit(event, onNotifyComplete);
         }
 
-        function createEvent() {
-          var eventObj = _.extend(params, {
-            images: _.map(images, function(image){
-              return new Image(image);
-            }),
-            author: currentUser._id
-          });
-          logger.debug('Creating event ', eventObj);
-          event = new Event(eventObj);
-          event.save(afterSave(event));
+        var respJson = {event: event};
+        logger.debug('Sending response', respJson);
+        flashFn('success', { msg: isCreate ? 'Ваше событие создано! Детали отправлены Вам на почту.': 'Ваше событие обновлено!' });
+        if (isCreate) {
+          flashFn('showFirstTime', 'true')
         }
-
-        function updateEvent() {
-          _.extend(event, params, {images: images.concat(imagesWithId)});
-
-          logger.debug('Updating event by id', event._id, event);
-          var respEvent = _.extend({}, event, {_id: event._id});
-          event.save(afterSave(respEvent));
-        }
-
-        function afterSave(event) {
-          return function(err) {
-            if (err) {
-              logger.error("error while saving event", err);
-              return cb(400, {error: err});
-            }
-
-            var onNotifyComplete = function(err, resp) {
-              logger.debug('Notification response: %s, error: %s', resp, err);
-            };
-            if (isCreate) {
-              notificationService.notifyAuthorOnCreate(event, onNotifyComplete)
-            } else {
-              notificationService.notifyParticipantOnEdit(event, onNotifyComplete);
-            }
-
-            var respJson = {event: event};
-            logger.debug('Sending response', respJson);
-            flashFn('success', { msg: isCreate ? 'Ваше событие создано! Детали отправлены Вам на почту.': 'Ваше событие обновлено!' });
-            if (isCreate) {
-              flashFn('showFirstTime', 'true')
-            }
-            cb(200, respJson);
-          }
-        }
-    };
+        cb(200, respJson);
+      }
+    }
+  };
 }
 
 function copyImage(image, next) {
-    logger.debug('copy image ', image);
+  logger.debug('copy image ', image);
 
   // Siman: Image uploading works for me if comment next line. Does it work on Linux as well?
-//    var imagePath = path.join('/', image.name); // removes any '..'
-    var imagePath = path.join(UPLOAD_DIR, image.name);
+//  var imagePath = path.join('/', image.name); // removes any '..'
+  var imagePath = path.join(UPLOAD_DIR, image.name);
 
-    fs.exists(imagePath, function(exists) {
-        logger.debug('Image ', imagePath, 'exists ', exists);
-        logger.debug('uploadDir ', UPLOAD_DIR);
-        if (exists && imagePath.indexOf(UPLOAD_DIR) == 0) {
-          maybeCreateImgDir(EVENT_IMG_DIR, config.PERSISTENT_DATA_DIR, function(err) {
-            if (err) return next(err);
-            utils.moveFile(imagePath, EVENT_IMG_DIR, function(err, newPath) {
-                next(err, _.extend(image, {name: path.basename(newPath)}));
-            });
-          });
-        } else {
-            next(new Error('Image name is invalid'));
-        }
-    });
+  fs.exists(imagePath, function (exists) {
+    logger.debug('Image ', imagePath, 'exists ', exists);
+    logger.debug('uploadDir ', UPLOAD_DIR);
+    if (exists && imagePath.indexOf(UPLOAD_DIR) == 0) {
+      maybeCreateImgDir(EVENT_IMG_DIR, config.PERSISTENT_DATA_DIR, function (err) {
+        if (err) return next(err);
+        utils.moveFile(imagePath, EVENT_IMG_DIR, function (err, newPath) {
+          next(err, _.extend(image, {name: path.basename(newPath)}));
+        });
+      });
+    } else {
+      next(new Error('Image name is invalid'));
+    }
+  });
 }
 
 function maybeCreateImgDir(imgDir, persistentDir, next) {
