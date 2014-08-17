73f0dd7328e81d4b8b530f73e3a3e234af459bfd
Alex Siman <aleksandr.siman@gmail.com>
2014-08-17 18:55:42 +0300

    Find files for generation.

--- app/controllers/dev/generator.js
+++ app/controllers/dev/generator.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var logger = require('../util/logger')(__filename);
+var fs = require('fs-extra');
+var path = require('path');
+var async = require('async');
 
 module.exports.view = function(req, res, next) {
   res.render('dev/generator', { title: 'Mock Generator' });
@@ -12,6 +15,50 @@ module.exports.generate = function(req, res, next) {
   logger.debug('Gen params', params);
 
   // TODO Impl
-  
-  res.send(200);
+  generateEvent({params: params}, function(err, event) {
+    if (err) {
+      logger.error('Failed to generate events', err);
+      return res.json(500, err);
+    }
+    res.send(200);
+  });
 };
+
+/**
+ * @param args
+ * @param args.params
+ * @param {Function} cb function(err, event)
+ */
+function generateEvent(args, cb) {
+  findFiles('descriptions', function(err, files) {
+    cb(err);
+  });
+}
+
+/**
+ * @param {String} typeOfFiles possible values: ['descriptions', 'images']
+ * @param cb
+ */
+function findFiles(typeOfFiles, cb) {
+  var staticDir = path.join(process.cwd(), 'app/generator/static');
+  async.waterfall([
+    function readActDirs(cb) {
+      fs.readdir(staticDir, cb);
+    },
+    function readFiles(actDirs, cb) {
+      logger.debug('actDirs', actDirs);
+      async.reduce(actDirs, [], function(memo, file, cb) {
+        fs.readdir(staticDir + '/' + file + '/' + typeOfFiles, function(err, files) {
+          if (err && err.code == 'ENOENT') {
+            logger.warn('Cannot find ' + typeOfFiles + ' for', file);
+            return cb(null, memo);
+          }
+          cb(err, memo.concat(files));
+        });
+      }, cb);
+    }
+  ], function(err, files) {
+    logger.debug('Found mock files', files);
+    return cb(err, files);
+  });
+}
