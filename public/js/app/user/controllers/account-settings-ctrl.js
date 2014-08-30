7cb5ba0ccb87fc6f6a6c93856785eb432b1f3b23
oleksandr <oleksandr.bezhan@gmail.com>
2014-08-30 22:27:00 +0300

    Nice alert for accont settings page.

--- public/js/app/user/controllers/account-settings-ctrl.js
+++ public/js/app/user/controllers/account-settings-ctrl.js
@@ -3,12 +3,13 @@
  * Created by oleksandr at 8/10/14 5:42 PM
  */
 angular.module('myApp')
-  .controller('AccountSettingsCtrl', ['$scope', 'activities', '$http', 'util', 'ErrorService', function($scope, activities, $http, util, ErrorService) {
+  .controller('AccountSettingsCtrl', ['$scope', 'activities', '$http', 'util', 'ErrorService', '$alert',
+    function($scope, activities, $http, util, ErrorService, $alert) {
     $scope.activities = activities;
     $scope.msg = [];
     $scope.updateProfile = function() {
       $http.post(util.apiUrl('/user/updateProfile'), $scope.currentUser).success(function(res) {
-        $scope.msg = 'Изменения сохранены.';
+        $alert({content: 'Изменения сохранены.'});
       }).error(function(res) {
         ErrorService.handleResponse(res);
       });
