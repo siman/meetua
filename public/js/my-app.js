a1cec01166b3986a3e65705cde75707f41b8fa3b
oleksandr <oleksandr.bezhan@gmail.com>
2014-08-30 16:33:12 +0300

    Improve prettyDateTime angular filter

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -54,7 +54,11 @@ angular.module('myApp', [
   })
   .filter('prettyDateTime', function() {
     return function(date) {
-      return moment(date).format('dddd Do MMMM HH:mm');
+      if (date) {
+        return moment(date).format('dddd Do MMMM HH:mm');
+      } else {
+        console.warn('prettyDateTime arg is undefined');
+      }
     };
   });
 
