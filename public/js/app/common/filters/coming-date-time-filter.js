212e70b8c6f3aaf503be01cb0f18bdeb1913908c
oleksandr <oleksandr.bezhan@gmail.com>
2014-10-04 00:33:05 +0300

    Show also time when date is after week.

--- public/js/app/common/filters/coming-date-time-filter.js
+++ public/js/app/common/filters/coming-date-time-filter.js
@@ -4,7 +4,9 @@ angular.module('myApp')
 .filter('comingDateTime', function() {
     return function(date) {
       if (date) {
-        return moment(date).calendar();
+        var d = moment(date);
+        var isAfterWeek = d.isAfter(moment().add(1, 'week'));
+        return d.calendar() + (isAfterWeek ? ', ' + d.format('hh:mm') : '');
       } else {
         console.warn('comingDate arg is undefined');
       }
